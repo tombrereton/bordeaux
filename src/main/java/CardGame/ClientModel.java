972d2da9bcd80d1733f22774b2786cd11199b250
lcarswell1 <lcarswell1@gmail.com>
2017-03-12 00:33:45 +0000

    add comments to code.

--- src/main/java/CardGame/ClientModel.java
+++ src/main/java/CardGame/ClientModel.java
@@ -7,7 +7,11 @@ import java.util.Observable;
 
 import CardGame.Responses.ResponseLoginUser;
 import CardGame.Responses.ResponseRegisterUser;
-
+/**
+ * The observable class that contains all the information to be displayed on the client gui and methods for sending to server.
+ * @author Lloyd
+ *
+ */
 public class ClientModel extends Observable {
 	//connectors
 	CardGameClient cardGameClient = new CardGameClient();
@@ -17,12 +21,18 @@ public class ClientModel extends Observable {
 	
 	//fields
 	User user;
-	
+	/**
+	 * Constructor.
+	 */
 	public ClientModel(){
 		this.connected = false;
 		this.loggedIn = false;
 	}
-	
+	/**
+	 * method that sends login request to server and updates loggedin and user fields.
+	 * @param username
+	 * @param password
+	 */
 	public void login(String username, String password){
 		User user = new User(username,password);
 		try {
@@ -36,14 +46,24 @@ public class ClientModel extends Observable {
 			e.printStackTrace();
 		}
 	}
-	
+	/**
+	 * helper method for login.
+	 * @param bool
+	 * @param user
+	 */
 	public void setLoggedIn(boolean bool, User user){
 		this.loggedIn = bool;
 		this.user = user;
 		setChanged();
 		notifyObservers(loggedIn);
 	}
-	
+	/**
+	 * Method for sending a request to register a user.
+	 * @param username
+	 * @param password
+	 * @param first
+	 * @param last
+	 */
 	public void registerUser(String username, String password, String first, String last){
 		User user = new User(username,password,first,last);
 		try {
@@ -56,19 +76,31 @@ public class ClientModel extends Observable {
 		
 		
 	}
-
+/**
+ * getter for cardgameclient object.
+ * @return
+ */
 	public CardGameClient getCardGameClient() {
 		return cardGameClient;
 	}
-
+/**
+ * getter for connected
+ * @return
+ */
 	public boolean isConnected() {
 		return connected;
 	}
-
+/**
+ * getter for boolean logged in
+ * @return
+ */
 	public boolean isLoggedIn() {
 		return loggedIn;
 	}
-
+/**
+ * getter for user
+ * @return
+ */
 	public User getUser() {
 		return user;
 	}
