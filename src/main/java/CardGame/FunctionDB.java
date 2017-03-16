9e4935fac00de6aeb9a6149d6c65355479aefb71
lholman <lholman@10.21.2.172>
2017-03-16 17:29:09 +0000

    additional methods in functions DB

--- src/main/java/CardGame/FunctionDB.java
+++ src/main/java/CardGame/FunctionDB.java
@@ -1,5 +1,6 @@
 package CardGame;
 
+
 import java.sql.*;
 
 /**
@@ -25,16 +26,16 @@ import java.sql.*;
  * <p>
  * CREATE TABLE game_outcomes (
  * outcomeID SERIAL,
- * outcome VARCHAR(100) NOT NULL UNIQUE,
+ * outcome VARCHAR(100) NOT NULL,
  * CONSTRAINT game_outcomes_key PRIMARY KEY (outcomeID)
  * );
  */
-
+// for Tom's DB, game_outcomes table needs recreating, as per above code
 
 /**
  * The DAO of the card game
  *
- * @author Yifan Wu
+ * @author Yifan Wu and Lois Holman
  * @version 2017-02-26
  */
 public class FunctionDB {
@@ -51,7 +52,8 @@ public class FunctionDB {
         try {
             Class.forName(DRIVER);
             try {
-                con = DriverManager.getConnection(URL, USER, PASS);
+              con = DriverManager.getConnection(URL, USER, PASS);
+                
                 System.out.println("Connected to database");
             } catch (SQLException e) {
                 e.printStackTrace();
@@ -166,7 +168,7 @@ public class FunctionDB {
         return update > 0;
     }
 
-    public synchronized int insertGameOutcomeIntoDatabse(String outcomes) throws SQLException {
+    public synchronized int insertGameOutcomeIntoDatabase(String outcomes) throws SQLException {
         String sql = "INSERT INTO game_outcomes(outcome) SELECT ? WHERE NOT EXISTS (SELECT 1 FROM game_outcomes WHERE outcome=?) RETURNING outcome_id";
         PreparedStatement stat = con.prepareStatement(sql);
         stat.setString(1, outcomes);
@@ -177,11 +179,11 @@ public class FunctionDB {
             rs.close();
             return result;
         }
-        // if any else happen throw a wrong result
+        // if any else happens throw a wrong result
         return -1;
     }
 
-    public synchronized boolean updateGameOutcomeFromDatabse(int outcome_id, String outcomes) throws SQLException {
+    public synchronized boolean updateGameOutcomeFromDatabase(int outcome_id, String outcomes) throws SQLException {
         String sql = "UPDATE game_outcomes SET outcomes = (?) WHERE outcoms_id = (?)";
         PreparedStatement stat = con.prepareStatement(sql);
         stat.setInt(1, outcome_id);
@@ -193,7 +195,7 @@ public class FunctionDB {
 
     public synchronized boolean insertGameStatsIntoDatabase(int gameid, int userid, int chip_amount_changed) throws SQLException {
         // initialise the outcome result
-        int outcomeid = insertGameOutcomeIntoDatabse("");
+        int outcomeid = insertGameOutcomeIntoDatabase("");
         String sql = "INSERT INTO users_games(user_id,game_id,outcom_id,chip_amount_changed) VALUES(?,?,?,?)";
         PreparedStatement stat = con.prepareStatement(sql);
         stat.setInt(1, userid);
@@ -205,4 +207,80 @@ public class FunctionDB {
     }
 
 
-}
+    
+    public synchronized int retrieveGamesWon(String username) throws SQLException{
+    	String sql =  "SELECT COUNT(*) AS rowcount FROM users_games JOIN game_outcomes ON game_outcomes.outcomeid = users_games.outcomeid  JOIN users ON users.userid = users_games.userid WHERE outcome LIKE 'win' AND username = ?;";
+    			PreparedStatement stat = con.prepareStatement(sql);		
+    	ResultSet rs = stat.executeQuery();
+    	// result.next();
+    	int count = rs.getInt("rowcount");
+    	// result.close();
+    	return count;
+    }
+    // need test data to see if this works
+    
+  
+    public synchronized int retrieveGamesLost(String username) throws SQLException{
+    	String sql =  "SELECT COUNT(*) AS rowcount FROM users_games JOIN game_outcomes ON game_outcomes.outcomeid = users_games.outcomeid  JOIN users ON users.userid = users_games.userid WHERE outcome LIKE 'lose' AND username = ?;";
+    			PreparedStatement stat = con.prepareStatement(sql);		
+    	ResultSet rs = stat.executeQuery();
+    	// result.next();
+    	int count = rs.getInt("rowcount");
+    	// result.close();
+    	return count;
+    }
+    
+    
+    public synchronized double percentageWins(String username) throws SQLException{
+    	double result = (retrieveGamesWon(username))/(retrieveGamesWon(username)+retrieveGamesLost(username));
+    	return result*100;
+    }
+    
+    
+    // get credit value of the very last line entered for that specific user. 
+    public synchronized int userCredit(){
+    	return 0;
+    }
+     
+}    
+    
+    
+/*
+ *     
+
+    Data to populate tables for testing 
+    
+    INSERT INTO users VALUES (11, 'bossman', 'password', 'Lloyd', 'Carswell');
+    INSERT INTO users VALUES (22, 'bombadil', 'password', 'Tom', 'Brereton');
+    INSERT INTO users VALUES (33, 'sprog', 'password', 'Lois', 'Holman');
+    INSERT INTO users VALUES (44, 'princess', 'password', 'Alex', 'Davenport');
+    INSERT INTO users VALUES (77, 'winner', 'password', 'Yifan', 'Woo');
+    
+    INSERT INTO game_outcomes VALUES (21, 'win');
+    INSERT INTO game_outcomes VALUES (22, 'win');
+    INSERT INTO game_outcomes VALUES (23, 'win');
+    INSERT INTO game_outcomes VALUES (24, 'lose');
+    INSERT INTO game_outcomes VALUES (25, 'lose');
+    INSERT INTO game_outcomes VALUES (26, 'lose');
+    INSERT INTO game_outcomes VALUES (27, 'win');
+    INSERT INTO game_outcomes VALUES (28, 'win');
+    INSERT INTO game_outcomes VALUES (29, 'lose');
+    INSERT INTO game_outcomes VALUES (30, 'win');
+    
+    INSERT INTO users_games VALUES (1, 11, 1, 21, 1);
+    INSERT INTO users_games VALUES (2, 22, 1, 22, 20);
+    INSERT INTO users_games VALUES (3, 33, 1, 23, 0);
+    INSERT INTO users_games VALUES (4, 44, 1, 24, 50);
+    INSERT INTO users_games VALUES (5, 11, 2, 25, 20);
+    INSERT INTO users_games VALUES (6, 22, 2, 26, 25);
+    INSERT INTO users_games VALUES (7, 33, 2, 27, 70);
+    INSERT INTO users_games VALUES (8, 44, 2, 28, 6);
+    INSERT INTO users_games VALUES (9, 11, 3, 29, 20);
+    INSERT INTO users_games VALUES (10, 22, 3, 30, 0);
+    
+ */
+
+
+
+
+
---
+++ src/test/java/CardGame/FunctionDBTest.java
@@ -0,0 +1,22 @@
+package CardGame;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * This class tests the database functions. Retrieving, querying and entering values into the database.
+ * @author lholman
+ * 
+ *
+ */
+
+
+public class FunctionDBTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
