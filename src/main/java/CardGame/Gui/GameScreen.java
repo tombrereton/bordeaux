cb98b045ca291da9f9f5222ea1326adda3609528
AJDavenport <AJDavenport@hotmail.co.uk>
2017-03-16 11:36:50 +0000

    updates to GameScreen Gui and to resources folder

--- src/main/java/CardGame/Gui/GameScreen.java
+++ src/main/java/CardGame/Gui/GameScreen.java
@@ -5,9 +5,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.Graphics;
-import java.awt.image.BufferedImage;
-import java.io.File;
 
 
 /**
@@ -18,7 +15,6 @@ import java.io.File;
 public class GameScreen extends JPanel {
 
 	private JTextArea textArea;
-	private static Graphics g;
 
 	/**
 	 * Create the application.
@@ -32,14 +28,15 @@ public class GameScreen extends JPanel {
 	 */
 	private void initialize() {
 
-		setSize(1024,576);
-		setLayout(null);
+		//setSize(1024,576);
+		//setLayout(null);
+		//ScreenFactory.setPanelToFrame();
 
 		/**
 		 * Chat message box as a JScroll Pane
 		 */
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(809, 41, 181, 373);
+		scrollPane.setBounds(845, 40, 160, 340);
 		add(scrollPane);
 
 		/**
@@ -47,20 +44,26 @@ public class GameScreen extends JPanel {
 		 */
 		JLabel lblChat = new JLabel("Chat");
 		lblChat.setFont(new Font("Soho Std", Font.PLAIN, 18));
-		lblChat.setBounds(809, 11, 205, 35);
+		lblChat.setBounds(845, 10, 205, 35);
 		add(lblChat);
 
 		/**
 		 * Send message button
 		 */
-		JButton btnSendMessage = new JButton("Send");
+		JButton btnSendMessage = new JButton();
 		btnSendMessage.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnSendMessage.setBackground(Color.WHITE);
-		btnSendMessage.setFont(new Font("Soho Std", Font.PLAIN, 18));
-		btnSendMessage.setBounds(809, 498, 181, 29);
+		btnSendMessage.setBounds(845, 470, 159, 60);
+		btnSendMessage.setContentAreaFilled(false);
+		btnSendMessage.setBorderPainted(false);
+		try {
+			Image imgSendMessage = ImageIO.read(getClass().getResource("/gameHud/imageBtnMessage.png"));
+			btnSendMessage.setIcon(new ImageIcon(imgSendMessage));
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}
 		add(btnSendMessage);
 
 		/**
@@ -68,7 +71,7 @@ public class GameScreen extends JPanel {
 		 */
 		textArea = new JTextArea();
 		textArea.setLineWrap(true);
-		textArea.setBounds(809, 425, 181, 62);
+		textArea.setBounds(845, 400, 160, 60);
 		add(textArea);
 		textArea.setColumns(10);
 
@@ -77,87 +80,134 @@ public class GameScreen extends JPanel {
 		 */
 
 		//DoubleDown Button
-		JButton btnDoubledown = new JButton("Double Down");
+		JButton btnDoubledown = new JButton();
 		btnDoubledown.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnDoubledown.setBackground(Color.WHITE);
-		btnDoubledown.setFont(new Font("Soho Std", Font.PLAIN, 12));
-		btnDoubledown.setBounds(675, 492, 80, 35);
+		btnDoubledown.setBounds(740, 470, 98, 55);
+		btnDoubledown.setContentAreaFilled(false);
+		btnDoubledown.setBorderPainted(false);
+		try {
+			Image imgDoubleDown = ImageIO.read(getClass().getResource("/gameHud/imageBtnDoubleDown.png"));
+			btnDoubledown.setIcon(new ImageIcon(imgDoubleDown));
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}
 		add(btnDoubledown);
 
 		//Stand Button
-		JButton btnStand = new JButton("Stand");
+		JButton btnStand = new JButton();
 		btnStand.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnStand.setBackground(Color.WHITE);
-		btnStand.setFont(new Font("Soho Std", Font.PLAIN, 12));
-		btnStand.setBounds(585, 493, 80, 35);
+		btnStand.setBounds(640, 470, 98, 55);
+		btnStand.setContentAreaFilled(false);
+		btnStand.setBorderPainted(false);
+		try {
+			Image imgStand = ImageIO.read(getClass().getResource("/gameHud/imageBtnStand.png"));
+			btnStand.setIcon(new ImageIcon(imgStand));
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}
 		add(btnStand);
 
 		//Hit Button
-		JButton btnHit = new JButton("Hit");
+		JButton btnHit = new JButton();
 		btnHit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnHit.setFont(new Font("Soho Std", Font.PLAIN, 12));
-		btnHit.setBackground(Color.WHITE);
-		btnHit.setBounds(495, 492, 80, 35);
+		btnHit.setBounds(540, 470, 98, 55);
+		btnHit.setContentAreaFilled(false);
+		btnHit.setBorderPainted(false);
+		try {
+			Image imgHit = ImageIO.read(getClass().getResource("/gameHud/imageBtnHit.png"));
+			btnHit.setIcon(new ImageIcon(imgHit));
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}
 		add(btnHit);
 
 		//Hold Button
-		JButton btnFold = new JButton("Fold");
+		JButton btnFold = new JButton();
 		btnFold.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnFold.setFont(new Font("Soho Std", Font.PLAIN, 12));
-		btnFold.setBackground(Color.WHITE);
-		btnFold.setBounds(405, 492, 80, 35);
+		btnFold.setBounds(440, 470, 98, 55);
+		btnFold.setContentAreaFilled(false);
+		btnFold.setBorderPainted(false);
+		try {
+			Image imgFold = ImageIO.read(getClass().getResource("/gameHud/imageBtnFold.png"));
+			btnFold.setIcon(new ImageIcon(imgFold));
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}
 		add(btnFold);
 
 		/**
 		 * Pot label
 		 */
-		JLabel lblNewLabel = new JLabel("Pot:");
-		lblNewLabel.setFont(new Font("Soho Std", Font.PLAIN, 18));
-		lblNewLabel.setBounds(383, 149, 46, 29);
-		add(lblNewLabel);
+//		JLabel lblNewLabel = new JLabel("Pot:");
+//		lblNewLabel.setFont(new Font("Soho Std", Font.PLAIN, 18));
+//		lblNewLabel.setBounds(383, 149, 46, 29);
+//		add(lblNewLabel);
 
 		/**
 		 * Player credits label
 		 */
 		JLabel lblCredits = new JLabel("Credits: \u00A31000");
 		lblCredits.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		lblCredits.setForeground(Color.WHITE);
-		lblCredits.setBounds(30, 500, 92, 35);
+		lblCredits.setForeground(Color.BLACK);
+		lblCredits.setBounds(30, 495, 92, 35);
 		add(lblCredits);
+
+		/**
+		 * Player credits to bet label
+		 */
+		JLabel lblSubmitBet = new JLabel("Bet to be placed");
+		lblSubmitBet.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblSubmitBet.setForeground(Color.BLACK);
+		lblSubmitBet.setBounds(30, 455, 92, 35);
+		add(lblSubmitBet);
+
+		//Submit Bet
+		JButton btnSubmitBet = new JButton();
+		btnSubmitBet.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnSubmitBet.setBounds(5, 350, 98, 91);
+		btnSubmitBet.setContentAreaFilled(false);
+		btnSubmitBet.setBorderPainted(false);
+		try {
+			Image imgSubmitBet = ImageIO.read(getClass().getResource("/gameHud/imageBtnBet.png"));
+			btnSubmitBet.setIcon(new ImageIcon(imgSubmitBet));
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}
+		add(btnSubmitBet);
 
 		/**
 		 * Bet Buttons
 		 */
-
 		//Bet 1: small
 		JButton btnBet1 = new JButton();
 		btnBet1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnBet1.setBounds(10, 380, 96, 96);
+		btnBet1.setBounds(105, 370, 81, 81);
 		btnBet1.setContentAreaFilled(false);
 		btnBet1.setBorderPainted(false);
 		try {
-			Image imgBet1 = ImageIO.read(getClass().getResource("/chips/chipBlack.png"));
+			Image imgBet1 = ImageIO.read(getClass().getResource("/gameHud/imageBtnBet1.png"));
 			btnBet1.setIcon(new ImageIcon(imgBet1));
 		} catch (Exception ex) {
 			System.out.println(ex);
 		}
-//		btnBet1.setFont(new Font("Soho Std", Font.PLAIN, 12));
-//		btnBet1.setForeground(Color.WHITE);
 		add(btnBet1);
 
 
@@ -167,11 +217,11 @@ public class GameScreen extends JPanel {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnBet2.setBounds(106, 405, 96, 96);
+		btnBet2.setBounds(186, 410, 81, 81);
 		btnBet2.setContentAreaFilled(false);
 		btnBet2.setBorderPainted(false);
 		try {
-			Image imgBet2 = ImageIO.read(getClass().getResource("/chips/chipBlue.png"));
+			Image imgBet2 = ImageIO.read(getClass().getResource("/gameHud/imageBtnBet2.png"));
 			btnBet2.setIcon(new ImageIcon(imgBet2));
 		} catch (Exception ex) {
 			System.out.println(ex);
@@ -184,11 +234,11 @@ public class GameScreen extends JPanel {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnBet3.setBounds(202, 420, 96, 96);
+		btnBet3.setBounds(267, 440, 81, 81);
 		btnBet3.setContentAreaFilled(false);
 		btnBet3.setBorderPainted(false);
 		try {
-			Image imgBet3 = ImageIO.read(getClass().getResource("/chips/chipPurple.png"));
+			Image imgBet3 = ImageIO.read(getClass().getResource("/gameHud/imageBtnBet3.png"));
 			btnBet3.setIcon(new ImageIcon(imgBet3));
 		} catch (Exception ex) {
 			System.out.println(ex);
@@ -201,11 +251,11 @@ public class GameScreen extends JPanel {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnBet4.setBounds(298, 430, 96, 96);
+		btnBet4.setBounds(353, 450, 81, 81);
 		btnBet4.setContentAreaFilled(false);
 		btnBet4.setBorderPainted(false);
 		try {
-			Image imgBet4 = ImageIO.read(getClass().getResource("/chips/chipRed.png"));
+			Image imgBet4 = ImageIO.read(getClass().getResource("/gameHud/imageBtnBet4.png"));
 			btnBet4.setIcon(new ImageIcon(imgBet4));
 		} catch (Exception ex) {
 			System.out.println(ex);
@@ -213,49 +263,59 @@ public class GameScreen extends JPanel {
 		add(btnBet4);
 
 		/**
-		 * Player positions
+		 * Player positions and cards
 		 */
-		JTextArea txtPlayer1 = new JTextArea();
-		txtPlayer1.setLineWrap(true);
-		txtPlayer1.setText("Player1\nUsername\nCard#1:\r\nCard#2:\nCard#3:\nCard#4:");
-		txtPlayer1.setEditable(false);
-		txtPlayer1.setBounds(96, 305, 92, 109);
-		add(txtPlayer1);
+//		JLabel lblDeck = new JLabel();
+//		lblDeck.setBounds(700, 100, 241, 42);
+//		try {
+//			Image imgDeck = ImageIO.read(getClass().getResource("/cards/000.png"));
+//			lblDeck.setIcon(new ImageIcon(imgDeck));
+//		} catch (Exception ex) {
+//			System.out.println(ex);
+//		}
+//		add(lblDeck);
 
-		JTextArea txtPlayer2 = new JTextArea();
-		txtPlayer2.setText("Player2\nUsername\nCard#1:\r\nCard#2:\nCard#3:\nCard#4:");
-		txtPlayer2.setLineWrap(true);
-		txtPlayer2.setEditable(false);
-		txtPlayer2.setBounds(233, 327, 92, 114);
-		add(txtPlayer2);
-
-		JTextArea txtPlayer3 = new JTextArea();
-		txtPlayer3.setText("Player3\nUsername\nCard#1:\r\nCard#2:\nCard#3:\nCard#4:");
-		txtPlayer3.setLineWrap(true);
-		txtPlayer3.setEditable(false);
-		txtPlayer3.setBounds(366, 344, 92, 114);
-		add(txtPlayer3);
-
-		JTextArea txtPlayer4 = new JTextArea();
-		txtPlayer4.setText("Player4\nUsername\nCard#1:\r\nCard#2:\nCard#3:\nCard#4:");
-		txtPlayer4.setLineWrap(true);
-		txtPlayer4.setEditable(false);
-		txtPlayer4.setBounds(505, 327, 92, 114);
-		add(txtPlayer4);
-
-		JTextArea txtPlayer5 = new JTextArea();
-		txtPlayer5.setText("Player5\nUsername\nCard#1:\r\nCard#2:\nCard#3:\nCard#4:");
-		txtPlayer5.setLineWrap(true);
-		txtPlayer5.setEditable(false);
-		txtPlayer5.setBounds(646, 305, 92, 109);
-		add(txtPlayer5);
-
-		JTextArea txtDealer = new JTextArea();
-		txtDealer.setText("Dealer\nCard#1:\r\nCard#2:\nCard#3:\nCard#4:");
-		txtDealer.setLineWrap(true);
-		txtDealer.setEditable(false);
-		txtDealer.setBounds(163, 41, 92, 97);
-		add(txtDealer);
+//		JTextArea txtPlayer1 = new JTextArea();
+//		txtPlayer1.setLineWrap(true);
+//		txtPlayer1.setText("Player1\nUsername\nCard#1:\r\nCard#2:\nCard#3:\nCard#4:");
+//		txtPlayer1.setEditable(false);
+//		txtPlayer1.setBounds(96, 305, 92, 109);
+//		add(txtPlayer1);
+//
+//		JTextArea txtPlayer2 = new JTextArea();
+//		txtPlayer2.setText("Player2\nUsername\nCard#1:\r\nCard#2:\nCard#3:\nCard#4:");
+//		txtPlayer2.setLineWrap(true);
+//		txtPlayer2.setEditable(false);
+//		txtPlayer2.setBounds(233, 327, 92, 114);
+//		add(txtPlayer2);
+//
+//		JTextArea txtPlayer3 = new JTextArea();
+//		txtPlayer3.setText("Player3\nUsername\nCard#1:\r\nCard#2:\nCard#3:\nCard#4:");
+//		txtPlayer3.setLineWrap(true);
+//		txtPlayer3.setEditable(false);
+//		txtPlayer3.setBounds(366, 344, 92, 114);
+//		add(txtPlayer3);
+//
+//		JTextArea txtPlayer4 = new JTextArea();
+//		txtPlayer4.setText("Player4\nUsername\nCard#1:\r\nCard#2:\nCard#3:\nCard#4:");
+//		txtPlayer4.setLineWrap(true);
+//		txtPlayer4.setEditable(false);
+//		txtPlayer4.setBounds(505, 327, 92, 114);
+//		add(txtPlayer4);
+//
+//		JTextArea txtPlayer5 = new JTextArea();
+//		txtPlayer5.setText("Player5\nUsername\nCard#1:\r\nCard#2:\nCard#3:\nCard#4:");
+//		txtPlayer5.setLineWrap(true);
+//		txtPlayer5.setEditable(false);
+//		txtPlayer5.setBounds(646, 305, 92, 109);
+//		add(txtPlayer5);
+//
+//		JTextArea txtDealer = new JTextArea();
+//		txtDealer.setText("Dealer\nCard#1:\r\nCard#2:\nCard#3:\nCard#4:");
+//		txtDealer.setLineWrap(true);
+//		txtDealer.setEditable(false);
+//		txtDealer.setBounds(163, 41, 92, 97);
+//		add(txtDealer);
 
 		JButton btnLeaveGame = new JButton("Leave Game");
 		btnLeaveGame.addActionListener(new ActionListener() {
@@ -265,21 +325,49 @@ public class GameScreen extends JPanel {
 		});
 		btnLeaveGame.setBackground(Color.WHITE);
 		btnLeaveGame.setFont(new Font("Soho Std", Font.PLAIN, 12));
-		btnLeaveGame.setBounds(886, 7, 104, 23);
+		btnLeaveGame.setBounds(900, 7, 104, 23);
 		add(btnLeaveGame);
 
 
-	}
+		/**
+		 * Credits Box
+		 */
+		JLabel lblCreditsBox = new JLabel();
+		lblCreditsBox.setBounds(10, 490, 241, 42);
+		try {
+			Image imgBoxCredits = ImageIO.read(getClass().getResource("/gameHud/imageBoxCredits.png"));
+			lblCreditsBox.setIcon(new ImageIcon(imgBoxCredits));
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}
+		add(lblCreditsBox);
 
-	/**
-	 * Paint Graphics
-	 */
-	@Override
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		Color hudColour = new Color(120, 44, 44);
-		g.setColor(hudColour);
-		g.fillRect(0, 480, 1024, 80);
+		/**
+		 * Bet Box
+		 */
+		JLabel lblBetBox = new JLabel();
+		lblBetBox.setBounds(10, 450, 144, 45);
+		try {
+			Image imgSubmitBox = ImageIO.read(getClass().getResource("/gameHud/imageBoxBet.png"));
+			lblBetBox.setIcon(new ImageIcon(imgSubmitBox));
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}
+		add(lblBetBox);
+
+		/**
+		 * Back hud
+		 */
+		JLabel lblBackHud = new JLabel();
+		lblBackHud.setBounds(-20, 400, 1034, 204);
+		try {
+			Image imgHud = ImageIO.read(getClass().getResource("/gameHud/imageHud.png"));
+			lblBackHud.setIcon(new ImageIcon(imgHud));
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}
+		add(lblBackHud);
+
 	}
 
 }
