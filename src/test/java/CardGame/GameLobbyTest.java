32d27ede74acbbab7572ec82b7ef813ef5eba8d1
Ivan <yifanb@gmail.com>
2017-03-20 13:41:32 +0000

    add additional Junit test to GameLobby include some part of gameEngine

--- src/test/java/CardGame/GameLobbyTest.java
+++ src/test/java/CardGame/GameLobbyTest.java
@@ -141,6 +141,62 @@ public class GameLobbyTest {
     }
 
 
+
+    /**
+     * Test if all player bet is placed
+     */
+    @Test
+    public void allPlayersBetPlacedTest1(){
+        GameLobby gameLobby = new GameLobby(user,s);
+
+        Player player1 = new Player(user);
+        Player player2 = new Player(user2);
+
+        gameLobby.addPlayer(user2,s);
+
+        gameLobby.getPlayer(user.getUserName()).setBetPlaced(true);
+        gameLobby.getPlayer(user2.getUserName()).setBetPlaced(true);
+
+        gameLobby.setAllPlayersBetPlaced(true);
+        assertEquals(true,gameLobby.isAllPlayersBetPlaced());
+
+    }
+    /**
+     * Test if all player is finished round
+     */
+    @Test
+    public void allPlayersBetPlacedTest2(){
+        GameLobby gameLobby = new GameLobby(user,s);
+
+        Player player1 = new Player(user);
+        User user2 = new User("TestUser2");
+        Player player2 = new Player(user2);
+
+        gameLobby.addPlayer(user,s);
+        gameLobby.addPlayer(user2,s);
+
+        gameLobby.getPlayer(user.getUserName()).setBetPlaced(true);
+        gameLobby.getPlayer(user2.getUserName()).setBetPlaced(false);
+
+        assertEquals(false,gameLobby.isAllPlayersBetPlaced());
+
+    }
+
+    /**
+     * Edge cases
+     */
+    @Test
+    public void allPlayersBetPlacedTest3(){
+        GameLobby gameLobby = new GameLobby(user,s);
+
+        assertEquals(false,gameLobby.isAllPlayersBetPlaced());
+    }
+
+
+
+    /**
+     * Test the bet part
+     */
     @Test
     public void takeBetsTest1(){
         GameLobby gameLobby = new GameLobby(user,s);
@@ -156,9 +212,11 @@ public class GameLobbyTest {
         gameLobby.getPlayer("TestUser2").setBet(20);
 
         assertEquals(20,gameLobby.getPlayer("TestUser2").getBet());
-
-
     }
+
+    /**
+     * Test the bet part
+     */
     @Test
     public void takeBetsTest2(){
         GameLobby gameLobby = new GameLobby(user,s);
@@ -175,6 +233,10 @@ public class GameLobbyTest {
 
 
     }
+
+    /**
+     * Test the bet part
+     */
     @Test
     public void takeBetsTest3(){
         GameLobby gameLobby = new GameLobby(user,s);
@@ -190,6 +252,48 @@ public class GameLobbyTest {
 
     }
 
+
+    /**
+     * Test the double bet part
+     */
+    @Test
+    public void takeDoubleBetsTest1(){
+        GameLobby gameLobby = new GameLobby(user,s);
+
+        Player player1 = new Player(user);
+        User user2 = new User("TestUser2");
+        Player player2 = new Player(user2);
+
+        gameLobby.addPlayer(user,s);
+
+        gameLobby.getPlayer("TestUser1").setBet(10);
+        gameLobby.getPlayer("TestUser1").doubleBet();
+
+        assertEquals(20,gameLobby.getPlayer("TestUser1").getBet());
+    }
+
+    /**
+     * Test the double bet part
+     */
+    @Test
+    public void takeDoubleBetsTest2(){
+        GameLobby gameLobby = new GameLobby(user,s);
+
+        Player player1 = new Player(user);
+
+        gameLobby.addPlayer(user,s);
+
+        gameLobby.getPlayer("TestUser1").doubleBet();
+
+        assertEquals(0,gameLobby.getPlayer("TestUser1").getBet());
+
+
+    }
+
+
+    /**
+     * Test the game part
+     */
     @Test
     public void dealtoDealerTest1(){
         GameLobby gameLobby = new GameLobby(user,s);
@@ -201,6 +305,9 @@ public class GameLobbyTest {
 
 
     }
+    /**
+     * Test the game part
+     */
     @Test
     public void playerTest(){
         GameLobby gameLobby = new GameLobby(user,s);
@@ -215,6 +322,93 @@ public class GameLobbyTest {
         System.out.println("Player 1: " +player1.getPlayerHand().getHand().get(1).getValue());
 
     }
+
+    /**
+     * Test the budget part
+     */
+    @Test
+    public void playerBudgetTest1(){
+        GameLobby gameLobby = new GameLobby(user,s);
+
+        Player player1 = new Player(user);
+        gameLobby.addPlayer(user,s);
+        gameLobby.getPlayers().get(0).setBudget(200);
+
+        assertEquals(200,gameLobby.getPlayers().get(0).getBudget());
+    }
+
+    /**
+     * Test the budget part
+     */
+    @Test
+    public void playerBudgetTest2(){
+        GameLobby gameLobby = new GameLobby(user,s);
+        Player player1 = new Player(user);
+        gameLobby.addPlayer(user,s);
+
+        assertEquals(100,gameLobby.getPlayers().get(0).getBudget());
+    }
+
+    /**
+     * Test the bust part
+     */
+    @Test
+    public void playerBustTest1(){
+        GameLobby gameLobby = new GameLobby(user,s);
+        Player player1 = new Player(user);
+
+        Deck deck = new Deck();
+        deck.shuffle();
+
+        Card card = deck.dealCard();
+
+        while(gameLobby.getPlayers().get(0).getPlayerHandValue() < 8){
+            deck.shuffle();
+            card = deck.dealCard();
+            gameLobby.getPlayers().get(0).addCardToPlayerHand(card);
+        }
+
+        System.out.println("Now the value of player's hand card :" + gameLobby.getPlayers().get(0).getPlayerHandValue());
+        assertEquals(false,gameLobby.getPlayers().get(0).isBust());
+
+    }
+
+    /**
+     * Test the bust part
+     */
+    @Test
+    public void playerBustTest2(){
+        GameLobby gameLobby = new GameLobby(user,s);
+        Player player1 = new Player(user);
+
+        Deck deck = new Deck();
+        deck.shuffle();
+
+        Card card = deck.dealCard();
+
+        while(gameLobby.getPlayers().get(0).getPlayerHandValue() <= 21){
+            deck.shuffle();
+            card = deck.dealCard();
+            gameLobby.getPlayers().get(0).addCardToPlayerHand(card);
+        }
+
+        System.out.println("Now the value of player's hand card :" + gameLobby.getPlayers().get(0).getPlayerHandValue());
+        assertEquals(true,gameLobby.getPlayers().get(0).isBust());
+
+    }
+
+    /**
+     * Test the bust part
+     */
+    @Test
+    public void playerBustTest3(){
+        GameLobby gameLobby = new GameLobby(user,s);
+        Player player1 = new Player(user);
+
+        assertEquals(false,gameLobby.getPlayers().get(0).isBust());
+
+    }
+
 
 
 }
