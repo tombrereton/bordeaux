2a43d73a41beb2ce17073063d104e6ac673a4aaa
tombrereton <tbrereton89@gmail.com>
2017-03-22 18:25:51 +0000

    Added protocol for all Players finished

--- src/main/java/CardGame/Pushes/PushAreAllPlayersFinished.java
+++ src/main/java/CardGame/Pushes/PushAreAllPlayersFinished.java
@@ -2,29 +2,27 @@ package CardGame.Pushes;
 
 import CardGame.Responses.ResponseProtocol;
 
-import java.util.Map;
-
 import static CardGame.ProtocolTypes.PUSH_PLAYERS_FINISHED;
 
 /**
  * Created by tom on 11/03/17.
  */
 public class PushAreAllPlayersFinished extends ResponseProtocol {
-    private Map<String, Boolean> playersFinished;
+    private boolean allPlayersFinished;
 
-    public PushAreAllPlayersFinished(int protocolID, int success, Map<String, Boolean> playersFinished) {
+    public PushAreAllPlayersFinished(int protocolID, int success, boolean playersFinished) {
         super(protocolID, PUSH_PLAYERS_FINISHED, success);
-        this.playersFinished = playersFinished;
+        this.allPlayersFinished = playersFinished;
     }
 
-    public Map<String, Boolean> getPlayersFinished() {
-        return playersFinished;
+    public boolean isAllPlayersFinished() {
+        return allPlayersFinished;
     }
 
     @Override
     public String toString() {
         return "PushAreAllPlayersFinished{" +
-                "playersFinished=" + playersFinished +
+                "allPlayersFinished=" + allPlayersFinished +
                 "} " + super.toString();
     }
 }
